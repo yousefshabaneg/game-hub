a9632dda20e8ee1b70132775ffba1320348f7266
Yousef Shaban <yousefshaban.eg@gmail.com>
2023-06-11 20:50:46 +0300

    Refactoring: Simplify Game Query.

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
-  genre: Genre | null;
-  platform: Platform | null;
+  genreId?: number;
+  platformId?: number;
   sortOrder: string;
   searchText: string;
 }
@@ -46,8 +46,10 @@ function App() {
       <Show above="lg">
         <GridItem area="aside" paddingX={5} paddingY={7}>
           <GenreList
-            selectedGenre={gameQuery.genre}
-            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenreId={gameQuery.genreId}
+            onSelectGenre={(genre) =>
+              setGameQuery({ ...gameQuery, genreId: genre.id })
+            }
           />
         </GridItem>
       </Show>
@@ -57,9 +59,9 @@ function App() {
           <Flex marginTop={5}>
             <Box marginRight={5}>
               <PlatformSelector
-                selectedPlatform={gameQuery.platform}
+                selectedPlatformId={gameQuery.platformId}
                 onSelectPlatform={(platform) =>
-                  setGameQuery({ ...gameQuery, platform })
+                  setGameQuery({ ...gameQuery, platformId: platform.id })
                 }
               />
             </Box>
