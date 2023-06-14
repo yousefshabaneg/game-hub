bba3547b85cf846d51601175050eeada65030665
Yousef Shaban <yousefshaban.eg@gmail.com>
2023-06-14 10:29:56 +0300

    Build Game Attributes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,8 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
 import SearchInput from "./SearchInput";
 import ColorModeSwitch from "./ColorModeSwitch";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,9 +1,13 @@
 import { Platform } from "../hooks/Platform";
+import { Genre } from "./Genre";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
   name: string;
   slug: string;
+  genres: Genre[];
+  publishers: Publisher[];
   description_raw: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
